b147cf1da4da2da45c00bf26ff470e6569335a40
Evgheniy <147694568+Mitcov9847@users.noreply.github.com>
2025-05-09 16:20:03 +0300

    Add files via upload

---
+++ src/main/java/com/example/librarymanagement/repository/AuthorDao.java
@@ -0,0 +1,12 @@
+package com.example.librarymanagement.repository;
+
+import com.example.librarymanagement.entity.Author;
+import java.util.List;
+
+public interface AuthorDao {
+    Author save(Author entity);
+    Author update(Author entity);
+    void delete(Long id);
+    Author findById(Long id);
+    List<Author> findAll();
+}
